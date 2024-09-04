f82ebfee292ca44137332641e1e64e146b4f4ccd
Ryenan <ryenanaragaorodrigues@gmail.com>
2024-09-04 18:10:16 -0300

    ada

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const port = 3702;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'adm.html'));
 });
 
 app.listen(3702, () => {
-console.log(`Servidor rodando em http://localhost:${3702}`);
+    console.log(`Servidor rodando em http://localhost:${3702}`);
 });

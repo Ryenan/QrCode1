5b72a2be08a1b95a0a302382851ea8158ed5d5a4
Ryenan <ryenanaragaorodrigues@gmail.com>
2024-09-10 15:03:30 -0300

    dada

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/administracao', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'adm.html'));
 });
 

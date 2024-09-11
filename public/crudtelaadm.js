ff4c5623dda5f798787f460b081efd4acdc0109f
Ryenan <ryenanaragaorodrigues@gmail.com>
2024-09-11 18:57:43 -0300

    dadsadadad

--- public/crudtelaadm.js
+++ public/crudtelaadm.js
@@ -27,3 +27,30 @@ function checkLogin(){
 
     }
 }
+
+Quagga.init({
+    inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector('#camera'),
+        constraints:{
+            facingMode: "environment"
+        }
+    },
+    decoder: {
+        readers: ["ean_reader", "code_128_reader"]
+    }
+}, function (err) {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log("Initialization finished. Ready to start");
+    Quagga.start();
+});
+
+Quagga.onDetected(function(data){
+    const code = data.codeResult.code;
+
+    document.getElementById('codigoDoProduto').textContent = code;
+});

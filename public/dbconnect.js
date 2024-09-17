6232538c248a30ba02239df2b434969a0609edac
Ryenan <ryenanaragaorodrigues@gmail.com>
2024-09-16 23:26:39 -0300

    ada

--- public/dbconnect.js
+++ public/dbconnect.js
@@ -1,7 +1,16 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('sistemadecadastro', 'root', 'aragao@7355608', {
+const sequelize = new Sequelize('sistemadecadastro', 'root', '123kopi321',{
     host: "localhost", 
     dialect: 'mysql'
 })
 
-sequelize.authenticate();
+sequelize.authenticate().then(function(){
+    console.log("Conectado com sucesso!")
+}).catch(function(erro){
+    console.log("Falha ao se conectar:" +erro)
+});
+
+const Login = sequelize.create('login', {
+
+}
+)

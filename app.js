633ca81e15f70a5031394f580a433e9b49d57f9d
Ryenan <ryenanaragaorodrigues@gmail.com>
2024-10-02 16:26:19 -0300

    tarde do 02

--- app.js
+++ app.js
@@ -9,13 +9,10 @@ require('dotenv').config();
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
 
 app.use('/products', productRoutes); 
-
 app.use('/auth', authRoutes);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -25,8 +22,3 @@ app.get('/', (req, res) => {
 sequelize.authenticate()
   .then(() => console.log('Conectado ao MySQL!'))
   .catch((error) => console.log('Erro ao conectar ao MySQL:', error));
-
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});

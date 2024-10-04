42c0f35882af315b3ebba88f07122effa40d5c89
Ryenan <ryenanaragaorodrigues@gmail.com>
2024-10-03 21:52:18 -0300

    AHTA

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sistemadecadastro', 'root', '123kopi321', {
-  host: '127.0.0.1',
-  dialect: 'mysql'
+const sequelize = new Sequelize('railway', "mysql.railway.internal"  ,'YsSBFKGjYljoLCpzrccmOzapUnqUkEki', {
+  host: 'mysql.railway.internal',
+  dialect: 'mysql',
+  PORT:'3306'
 });
 
 module.exports = sequelize;
